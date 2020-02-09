dea47b466efed78c37adfce8de494d5797bc6125
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2020-02-09 12:49:14 +0900

    rewrite as numpy style

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 class TestClass:
     """
-    Summary
+    This class is made to test sphinx!
     """
 
     def testfunc(self, x:float, y:float) -> float:
         """sum
 
-        Args:
-            x (float): 1st argument
-            y (float): 2nd argument
+        testfunc to sum two args
+        Parameters
+        ----------
+            x:float
+                1st argument
+            y:float
+                2nd argument
 
-        Returns:
-            float: sum result
+        Returns
+        -------------
+            float
+                sum result
+        Note
+        -----------
+        Here is note
 
-        Examples:
+        Examples
+        --------------
             >>> print(testfunc(2,5))
             7
         """
