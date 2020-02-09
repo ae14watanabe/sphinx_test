6ed815a5c9d46f57dd45d5ed2c4f0b1d3c725ca6
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2020-02-09 11:42:21 +0900

    create test class and docs

---
+++ main.py
@@ -0,0 +1,13 @@
+class TestClass:
+    """
+    Summary
+    """
+
+    def testfunc(self, x:double, y:double) -> double:
+        """sum
+
+        :param x:
+        :param y:
+        :return sum of x and y:
+        """
+        return x+y
